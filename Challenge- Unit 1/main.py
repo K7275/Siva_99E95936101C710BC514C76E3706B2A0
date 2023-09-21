4152c86878b35e66c67eec3b72bc502dc5d7aa36
K7275 <145669032+K7275@users.noreply.github.com>
2023-09-21 16:52:33 +0530

    Update main.py

--- Challenge- Unit 1/main.py
+++ Challenge- Unit 1/main.py
@@ -1,5 +1,3 @@
-# Python program to find the factorial of a number provided by the user
-# using recursion
 
 def factorial(x):
     """This is a recursive function
@@ -8,16 +6,13 @@ def factorial(x):
     if x == 1:
         return 1
     else:
-        # recursive call to the function
+        
         return (x * factorial(x-1))
 
 
-# change the value for a different result
 num = 7
 
-# to take input from the user
 # num = int(input("Enter a number: "))
 
-# call the factorial function
 result = factorial(num)
 print("The factorial of", num, "is", result)
